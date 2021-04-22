76230e68530d61118948809bb04238064f3ed42b
Aledeloss <Aledeloss@gmail.com>
2021-04-22 00:18:47 -0300

    fix en App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
 import React from "react";
 import "./App.css";
 import Routes from "./Routes";
-import { RecordsProvider } from "./contexts/RecordsContext";
 import { SowingsProvider } from "./contexts/SowingsContext";
 
 function App() {
   return (
     <div className="App">
       <SowingsProvider>
-        <RecordsProvider>
           <Routes />
-        </RecordsProvider>
       </SowingsProvider>
     </div>
   );
