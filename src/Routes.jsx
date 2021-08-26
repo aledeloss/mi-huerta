cc4e2c4773b0903a0aceb829af770b491023124d
Aledeloss <Aledeloss@gmail.com>
2021-08-26 18:42:28 -0300

    formatos

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,39 +1,37 @@
-import React from 'react';
+import React from "react";
 import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
-    useHistory
-} from 'react-router-dom';
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  useHistory,
+} from "react-router-dom";
 
-import Home from './pages/Home/Home';
-import Start from './pages/Start/Start';
-import Records from './pages/Records/Records';
-import About from './pages/About/About';
-import Detail from './pages/Detail/Detail';
-import Page404 from './pages/Page404/Page404';
-import NoResults from './pages/NoResults/NoResults';
-import ScrollToTop from './components/ScrollToTop/ScrollToTop';
+import Home from "./pages/Home/Home";
+import Start from "./pages/Start/Start";
+import Records from "./pages/Records/Records";
+import About from "./pages/About/About";
+import Detail from "./pages/Detail/Detail";
+import Page404 from "./pages/Page404/Page404";
+import NoResults from "./pages/NoResults/NoResults";
+import ScrollToTop from "./components/ScrollToTop/ScrollToTop";
 
 const Routes = () => {
+  const history = useHistory();
 
-    const history = useHistory();
-
-    return (
-        <Router>
-            <ScrollToTop/>
-            <Switch history={history}>
-                <Route exact path='/' component={Home}/>
-                <Route exact path='/about' component={About} />
-                <Route exact path='/start' component={Start} />
-                <Route exact path='/records' component={Records} />
-                <Route exact path='/noresults' component={NoResults} />
-                <Route exact path='/detail/:vegetable' component={Detail} />
-                <Route  component={Page404} />
-            </Switch>
-        </Router>
-
-    )
+  return (
+    <Router>
+      <ScrollToTop />
+      <Switch history={history}>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/about" component={About} />
+        <Route exact path="/start" component={Start} />
+        <Route exact path="/records" component={Records} />
+        <Route exact path="/noresults" component={NoResults} />
+        <Route exact path="/detail/:vegetable" component={Detail} />
+        <Route component={Page404} />
+      </Switch>
+    </Router>
+  );
 };
 
 export default Routes;
