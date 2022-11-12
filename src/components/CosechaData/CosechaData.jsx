637fe87ef5fcba58d2aa406f1e0c9e0e90ea8160
Alejandra De Los Santos <aledeloss@gmail.com>
2022-11-11 23:45:18 -0300

    Remove unsed code

--- src/components/CosechaData/CosechaData.jsx
+++ src/components/CosechaData/CosechaData.jsx
@@ -2,20 +2,24 @@ import React from 'react';
 import DayJS from 'react-dayjs';
 import './CosechaData.css';
 
-import 'bootstrap/dist/css/bootstrap.min.css';
+const cosechaData = ({ vegetable }) => {
+  const sowingDate = new Date();
+  const harvestBegin = (
+    <DayJS format='DD/MM/YY' add={{ days: vegetable.harvest.from }}>
+      {sowingDate}
+    </DayJS>
+  );
+  const harvestEnd = (
+    <DayJS format='DD/MM/YY' add={{ days: vegetable.harvest.to }}>
+      {sowingDate}
+    </DayJS>
+  );
 
-const cosechaData = ({vegetable}) => {
-    
-    const sowingDate = new Date();
-    const harvestBegin = <DayJS format="DD/MM/YY" add={ { days: vegetable.harvest.from } }>{sowingDate}</DayJS>
-    const harvestEnd = <DayJS format="DD/MM/YY" add={ { days: vegetable.harvest.to } }>{sowingDate}</DayJS>
-    
-    
-    return (
-        <div className="harvest-data-container">
-        Si sembrás hoy podrás cosechar entre el {harvestBegin} y el {harvestEnd}.
-        </div>
-        )
-}
+  return (
+    <div className='harvest-data-container'>
+      Si sembrás hoy podrás cosechar entre el {harvestBegin} y el {harvestEnd}.
+    </div>
+  );
+};
 
 export default cosechaData;
--- src/components/FilterTag/FilterTag.jsx
+++ src/components/FilterTag/FilterTag.jsx
@@ -1,21 +1,7 @@
-import "./FilterTag.scss";
-// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-// import { faTimes } from "@fortawesome/free-solid-svg-icons";
+import './FilterTag.scss';
 
 const FilterTag = ({ tagName, handleDeleteTagCLick }) => {
-
-    const handleClick = () => {
-        alert('click')
-    }
-
-  return (
-    <span className="filter-tag">
-      {tagName}
-      {/*<div className="filter-tag__delete-icon" onClick={handleClick}>
-         <FontAwesomeIcon icon={faTimes} />
-      </div> */}
-    </span>
-  );
+  return <span className='filter-tag'>{tagName}</span>;
 };
 
 export default FilterTag;
